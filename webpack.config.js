c81203e887d6b764641cd629393a15421ac02c45
Robert Flack <flackr@gmail.com>
2019-11-20 19:40:40 -0500

    Add webpack.config.js.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+    entry: ['babel-polyfill', 'whatwg-fetch'],
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+    }
+  };
+
+module.exports = [config];
